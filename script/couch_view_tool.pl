4070c6d19c2932899dd5d63d3a3904469d1141e0
jeremy <jeremy@marzhillstudios.com>
2008-11-29 14:19:13 -0600

    added a script to the Makefile.PL
    darcs-hash:20081129201913-f4041-295a6e87f6b5e299aef64dffe4ff2f5092b61708.gz

---
+++ script/couch_view_tool.pl
@@ -0,0 +1,27 @@
+#!perl
+use DB::CouchDB::Schema::Edit;
+use Getopt::Long;
+use Pod::Usage;
+
+my $editor = DB::CouchDB::Schema::Edit->new();
+
+#allow me to pas in my own connection info
+
+my ($host,$port,$db);
+
+my $opts = GetOptions (
+    "host=s" => \$host,
+    "port=i" => \$port,
+    "db=s"   => \$db,
+    "dsn=s"  => \$dsn
+);
+
+if ($host && $db) {
+    my %dbargs = (host => $host);
+    $dbargs{port} = $port if $port;
+    $dbargs{db} = $db if $db;
+    $editor->schema(DB::CouchDB::Schema->new(%dbargs));
+}
+
+$editor->run();
+
